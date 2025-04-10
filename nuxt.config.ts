36feff7a829400422025b1e7fba4cc34e57221ed
Paul Diepold <paul.diepold@yahoo.de>
2025-04-10 18:42:37 +0200

    setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon']
+  modules: ['@nuxt/ui', '@nuxt/icon'],
 })

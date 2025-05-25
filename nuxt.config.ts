50dc234a0d2f43a18fea47034fc307cbaf1a9e42
Paul Diepold <paul.diepold@outlook.de>
2025-05-25 15:22:21 +0200

    exclude cron endpoint from auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,6 +48,7 @@ export default defineNuxtConfig({
       callback: '/auth/callback',
       exclude: [
         '/',
+        '/api/cron',
         '/competitions/**',
         '/registrations/confirm',
         '/registrations/cancel',

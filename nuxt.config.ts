7111a5d04de2f29790a40a1a96829f0098e04ae9
Paul Diepold <paul.diepold@outlook.de>
2026-04-22 23:22:04 +0200

    fix(pwa): App-Icon für Android korrigieren

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -160,10 +160,11 @@ export default defineNuxtConfig({
       display: 'standalone',
       start_url: '/',
       icons: [
-        { src: '/pwa-64x64.png', sizes: '64x64', type: 'image/png' },
-        { src: '/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-        { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-        { src: '/maskable-icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+        { src: '/maskable-icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+        { src: '/maskable-icon-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+        { src: '/pwa-64x64.png', sizes: '64x64', type: 'image/png', purpose: 'any' },
+        { src: '/pwa-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+        { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
       ],
       shortcuts: [
         {
